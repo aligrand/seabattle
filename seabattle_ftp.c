0e4a1808d65ea73e7092487e7720205e2430b50e
aligrand <ali.workjob@outlook.com>
2023-02-14 11:33:29 +0330

    linux bugs fixed

--- seabattle_ftp.c
+++ seabattle_ftp.c
@@ -4,7 +4,7 @@ void call_upload(){
 #if WIN32
     char *cmd = "ftp -s:.\\ftpc\\WINftp-upload.txt";
 #elif __linux__
-    char *cmd = "pftp -n < ftpc/ftp-upload";
+    char *cmd = "ftp -p -n < ftpc/ftp-upload";
 #endif
 
     system(cmd);
@@ -14,7 +14,7 @@ void call_download(){
 #if WIN32
     char *cmd = "ftp -s:.\\ftpc\\WINftp-download.txt";
 #elif __linux__
-    char *cmd = "pftp -n < ftpc/ftp-download";
+    char *cmd = "ftp -p -n < ftpc/ftp-download";
 #endif
 
     system(cmd);
